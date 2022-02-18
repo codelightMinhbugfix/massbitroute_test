98ef79939dd21f27401565cc386088da1a6564ba
duongvt <duongvu124@gmail.com>
2022-02-18 14:42:54 +0700

    test

--- CodeLight/src/test/java/features/massbit_route/DecentralizedAPI.java
+++ CodeLight/src/test/java/features/massbit_route/DecentralizedAPI.java
@@ -55,7 +55,6 @@ public class DecentralizedAPI {
         decentralized_api_steps .should_be_able_to_login();
         decentralized_api_steps.should_be_able_to_create_api(name, blockchain, network);
         decentralized_api_steps.should_be_able_to_add_entrypoint("MASSBIT");
-        
         for(int i = 0; i < 2; i++){
             Thread.sleep(5000);
             decentralized_api_steps.should_be_able_to_send_api_request(blockchain);
