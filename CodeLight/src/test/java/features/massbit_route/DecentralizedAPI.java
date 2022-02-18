7866461aa2e2b06f7870c64b668383c2c2a50b9d
duongvt <duongvu124@gmail.com>
2022-02-18 14:50:05 +0700

    c

--- CodeLight/src/test/java/features/massbit_route/DecentralizedAPI.java
+++ CodeLight/src/test/java/features/massbit_route/DecentralizedAPI.java
@@ -51,12 +51,12 @@ public class DecentralizedAPI {
     @Test
     public void massbit_route_call_api() throws IOException, InterruptedException {
 
-        
         decentralized_api_steps.should_be_able_to_say_hello();
         decentralized_api_steps .should_be_able_to_login();
         decentralized_api_steps.should_be_able_to_create_api(name, blockchain, network);
         decentralized_api_steps.should_be_able_to_add_entrypoint("MASSBIT");
-        for(int i = 0; i < 2; i++){
+
+        for(int i = 0; i < 3; i++){
             Thread.sleep(5000);
             decentralized_api_steps.should_be_able_to_send_api_request(blockchain);
         }
