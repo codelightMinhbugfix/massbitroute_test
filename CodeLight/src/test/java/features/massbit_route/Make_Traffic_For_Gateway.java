2deae55bc8d422bf176fd3bdd4800f1833a5112c
duongvt <duongvu124@gmail.com>
2022-03-02 11:30:39 +0700

    update

--- CodeLight/src/test/java/features/massbit_route/Make_Traffic_For_Gateway.java
+++ CodeLight/src/test/java/features/massbit_route/Make_Traffic_For_Gateway.java
@@ -28,50 +28,50 @@ public class Make_Traffic_For_Gateway {
     @Steps
     Community_Nodes_Steps community_nodes_steps;
 
-    @Test
-    public void make_traffic_for_gateway() throws IOException, InterruptedException {
-
-        list_gateway = gateway_community_steps.get_all_gateway_in_massbit();
-
-        decentralized_api_steps.should_be_able_to_say_hello();
-        decentralized_api_steps.should_be_able_to_login();
-
-        for(int i = 0; i <5; i++) {
-
-
-            for (List<String> list : list_gateway) {
-                String blockchain = list.get(2);
-                String ip = list.get(4);
-                decentralized_api_steps.should_be_able_to_create_api("traffic_gateway_test", blockchain, "mainnet");
-                decentralized_api_steps.should_be_able_to_add_entrypoint("MASSBIT");
-
-                decentralized_api_steps.send_api_request(blockchain, ip);
-
-
-            }
-        }
-
-    }
-
 //    @Test
-//    public void make_traffic_for_nodes() throws IOException, InterruptedException {
+//    public void make_traffic_for_gateway() throws IOException, InterruptedException {
 //
-//        list_node = community_nodes_steps.get_all_node_in_massbit();
+//        list_gateway = gateway_community_steps.get_all_gateway_in_massbit();
 //
 //        decentralized_api_steps.should_be_able_to_say_hello();
 //        decentralized_api_steps.should_be_able_to_login();
 //
-//        for(List<String> list : list_node){
-//            String blockchain = list.get(2);
-//            String id = list.get(0);
-//            String x_api_key = list.get(7);
-//            decentralized_api_steps.should_be_able_to_create_api("traffic_node_test", blockchain, "mainnet");
-//            decentralized_api_steps.should_be_able_to_add_entrypoint("MASSBIT");
-//            for(int i=0; i<5; i++) {
-//                decentralized_api_steps.send_api_request(blockchain, id, x_api_key);
+//
+//            for (List<String> list : list_gateway) {
+//                String blockchain = list.get(2);
+//                String ip = list.get(4);
+//                decentralized_api_steps.should_be_able_to_create_api("traffic_gateway_test", blockchain, "mainnet");
+//                decentralized_api_steps.should_be_able_to_add_entrypoint("MASSBIT");
+//
+//                for(int i = 0; i <5; i++) {
+//                    decentralized_api_steps.send_api_request(blockchain, ip);
+//                }
+//
+//
 //            }
 //
-//        }
 //
 //    }
+
+    @Test
+    public void make_traffic_for_nodes() throws IOException, InterruptedException {
+
+        list_node = community_nodes_steps.get_all_node_in_massbit();
+
+        decentralized_api_steps.should_be_able_to_say_hello();
+        decentralized_api_steps.should_be_able_to_login();
+
+        for(List<String> list : list_node){
+            String blockchain = list.get(2);
+            String id = list.get(0);
+            String x_api_key = list.get(7);
+            decentralized_api_steps.should_be_able_to_create_api("traffic_node_test", blockchain, "mainnet");
+            decentralized_api_steps.should_be_able_to_add_entrypoint("MASSBIT");
+            for(int i=0; i<2; i++) {
+                decentralized_api_steps.send_api_request(blockchain, id, x_api_key);
+            }
+
+        }
+
+    }
 }
