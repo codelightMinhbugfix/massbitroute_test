b4a5abdd41509638b1100af2b614280f830fe0f1
Viet Tai <vuviettai@gmail.com>
2022-04-26 11:32:45 +0700

    update client script

--- terraforms/clients/run.sh
+++ terraforms/clients/run.sh
@@ -1,11 +1,15 @@
 #! /bin/bash
 source ../../credentials/.env
+prefix=$random
+ROOT=$(realpath $(dirname $(realpath $0))/)
+PROVIDER_DIR=$ROOT/../providers
+CREDENTIALS_PATH=$ROOT/../../credentials
 #login
 #-------------------------------------------
 # Log into Portal
 #-------------------------------------------
 _login() {
-  bearer=$(curl --location --request POST 'https://portal.massbitroute.dev/auth/login' --header 'Content-Type: application/json' \
+  bearer=$(curl -s --location --request POST 'https://portal.massbitroute.dev/auth/login' --header 'Content-Type: application/json' \
           --data-raw "{\"username\": \"$TEST_USERNAME\", \"password\": \"$TEST_PASSWORD\"}"| jq  -r ". | .accessToken")
 
   if [[ "$bearer" == "null" ]]; then
@@ -13,7 +17,7 @@ _login() {
     exit 1
   fi
 
-  userId=$(curl --location --request GET 'https://portal.massbitroute.dev/user/info' \
+  userId=$(curl -s --location --request GET 'https://portal.massbitroute.dev/user/info' \
   --header "Authorization: Bearer $bearer" \
   --header 'Content-Type: application/json' | jq  -r ". | .id")
   echo "User ID $userId"
@@ -24,6 +28,7 @@ _prepare_terraform() {
   thread=5
   connection=5
   duration=120s
+  outtf="$2/client.tf"
   request_rates="(50 100 200)"
   echo 'variable "project_prefix" {
     type        = string
@@ -38,7 +43,7 @@ _prepare_terraform() {
   }
   variable "client_machine_type" {
      type = string
-   }' > client.tf
+   }' > "$outtf"
   declare -A nodeIds
   declare -A nodeIps
   declare -A nodeKeys
@@ -47,7 +52,7 @@ _prepare_terraform() {
       nodeIds[${zoneCode,,}]=$nodeId
       nodeIps[${zoneCode,,}]=$ip
       nodeKeys[${zoneCode,,}]=$appId
-    done < <(tail "../providers/nodestatus.csv")
+    done < <(tail "$PROVIDER_DIR/$1/nodestatus.csv")
   declare -A gwIds
   declare -A gwIps
   declare -A gwKeys
@@ -56,40 +61,45 @@ _prepare_terraform() {
       gwIds[${zoneCode,,}]=$gatewayId
       gwIps[${zoneCode,,}]=$ip
       gwKeys[${zoneCode,,}]=$appId
-    done < <(tail ../providers/gatewaystatus.csv)
+    done < <(tail "$PROVIDER_DIR/$1/gatewaystatus.csv")
 
   while IFS="," read -r region cloudZone zone
   do
 #    #create dapi for each client
-    random=$(echo $RANDOM | md5sum | head -c 5)
+    random=$(echo $RANDOM | md5sum | head -c 2)
     testDapis+=( $random )
     #-------------------------------------------
     # Create dAPI
     #-------------------------------------------
-    create_dapi_response=$(curl -s --location --request POST 'https://portal.massbitroute.dev/mbr/d-apis' \
-      --header "Authorization: Bearer $bearer" \
-      --header 'Content-Type: application/json' \
-      --data-raw "{
-        \"name\": \"$projectName-$random\",
-        \"projectId\": \"$projectId\"
-    }")
-    apiId=$(echo $create_dapi_response | jq -r '. | .entrypoints[0].apiId')
-    appKey=$(echo $create_dapi_response | jq -r '. | .appKey')
-    dapiURL="https:\/\/$apiId.${blockchain}-mainnet.$domain\/$appKey"
-    create_dapi_status=$(echo $create_dapi_response | jq .status)
-    if [[ "$create_dapi_status" != "1" ]]; then
-      echo "Create new dAPI: Failed"
-      exit 1
+    if [ "x$dapiURL" == "x" ]; then
+      create_dapi_response=$(curl -s --location --request POST "https://portal.$domain/mbr/d-apis" \
+        --header "Authorization: Bearer $bearer" \
+        --header 'Content-Type: application/json' \
+        --data-raw "{
+          \"name\": \"$projectName-$random\",
+          \"projectId\": \"$projectId\"
+      }")
+      apiId=$(echo $create_dapi_response | jq -r '. | .entrypoints[0].apiId')
+      appKey=$(echo $create_dapi_response | jq -r '. | .appKey')
+      dapiURL="https:\/\/$apiId.${blockchain}-mainnet.$domain\/$appKey"
+      create_dapi_status=$(echo $create_dapi_response | jq .status)
+      if [[ "$create_dapi_status" != "1" ]]; then
+        echo "Create new dAPI: Failed"
+        exit 1
+      else
+        echo "Create new dAPI: Passed"
+      fi
     else
-      echo "Create new dAPI: Passed"
+      echo "dapiURL is defined"
     fi
+    _dapiURL=$(echo $dapiURL | sed "s|\/|\\\/|g")
     cat init.tpl | sed "s/\[\[BEARER\]\]/$bearer/g" \
               | sed "s/\[\[ZONE\]\]/${cloudZone,,}/g" \
               | sed "s/\[\[PROJECT_ID\]\]/$projectId/g" \
               | sed "s/\[\[PROJECT_NAME\]\]/${projectName}/g" \
               | sed "s/\[\[DOMAIN\]\]/$domain/g" \
               | sed "s/\[\[BLOCKCHAIN\]\]/$blockchain/g" \
-              | sed "s/\[\[DAPI_URL\]\]/$dapiURL/g" \
+              | sed "s/\[\[DAPI_URL\]\]/$_dapiURL/g" \
               | sed "s/\[\[NODE_ID\]\]/${nodeIds[${zone,,}]}/g" \
               | sed "s/\[\[NODE_KEY\]\]/${nodeKeys[${zone,,}]}/g" \
               | sed "s/\[\[NODE_IP\]\]/${nodeIps[${zone,,}]}/g" \
@@ -99,36 +109,58 @@ _prepare_terraform() {
               | sed "s/\[\[THREAD\]\]/$thread/g" \
               | sed "s/\[\[CONNECTION\]\]/$connection/g" \
               | sed "s/\[\[DURATION\]\]/$duration/g" \
-              | sed "s/\[\[REQUEST_RATES\]\]/$request_rates/g" > init_$random.sh
+              | sed "s/\[\[REQUEST_RATES\]\]/$request_rates/g" > "$2/init_$random.sh"
     cat client.template |  sed "s/\[\[REGION\]\]/$region/g" \
                         | sed  "s/\[\[ZONE\]\]/$cloudZone/g" \
                         | sed  "s/\[\[EMAIL\]\]/$email/g" \
-                        | sed "s/\[\[INIT_FILE\]\]/$random/g" >> client.tf
-  done < <(tail zonelist.csv)
+                        | sed "s/\[\[INIT_FILE\]\]/$random/g" >> $outtf
+  done < <(tail "$CREDENTIALS_PATH/zonelist-test.csv")
 }
-
+_prepare_env() {
+  echo "Create test in dir $1"
+  if [ ! -d "$ROOT/$1" ]; then
+    mkdir "$ROOT/$1"
+  else
+    rm $ROOT/$1/init_*.sh
+  fi
+  cp ./terraform.tfvars $ROOT/$1
+  cat provider.tf |  sed "s/\[\[CREDENTIALS_PATH\]\]/$credentialsPath/g"  > "$ROOT/$1/provider.tf"
+}
 _create_vms() {
+  cd $ROOT/$1
   sudo terraform init
   sudo terraform plan -var-file=./terraform.tfvars -out=client.plan
   sudo terraform apply client.plan
 }
-_clean_init_files() {
-  for file in ${testDapis[@]}; do
-    rm init_${file}.sh
-  done
-}
+#_clean_init_files() {
+#  for file in ${testDapis[@]}; do
+#    rm init_${file}.sh
+#  done
+#}
+#
+# $1 provider environment
+# $2 test environment
 _setup() {
+  if [ "x$1" == "x" ]; then
+    envdir=$prefix
+  else
+    envdir=$2
+  fi
+  _prepare_env $envdir
   _login
-  _prepare_terraform
-  _create_vms
+  _prepare_terraform $1 $envdir
+  _create_vms $envdir
 #  _clean_init_files
 }
 _clean() {
   _login
   echo "Cleaning up VMs: In Progress"
+  cd $1
   sudo terraform destroy
   if [[ "$?" != "0" ]]; then echo "Faile to execute: terraform destroy "; exit 1; fi
   echo "Cleaning up VMs: Passed"
+  cd ..
+  sudo rm -rf $1
 }
 
 $@
---
+++ tools/wrk/benchmark.sh
@@ -0,0 +1,157 @@
+#!/usr/bin/env bash
+source ./params.sh
+
+_get_dapi_session() {
+  #curl -s -X HEAD -I "$dapiURL"   --header 'Content-Type: application/json' | tr -d '\r' | sed -En 's/^location: (.*)/\1/p'
+  #return
+  #dapiURL=$(curl -s -X HEAD -I "$dapiURL"   --header 'Content-Type: application/json' | grep -i "location:" | cut -d':' -f2- | awk '{print $1}')
+  #echo "|$dapiURL|"
+  #return
+    #Get sessionUrl
+    _dapiURL=$(curl -s -X HEAD -I "$1"   --header 'Content-Type: application/json' | awk -F': ' '/^location:/{gsub(/[\s\r]+$/,"",$2);print $2}')
+    #Call sessionUrl to get dapiURL with session
+    #echo "|$dapiURL|"
+    _dapiURL=$(curl -s -X HEAD -I "$_dapiURL"   --header 'Content-Type: application/json' | awk -F': ' '/^location:/{gsub(/[\s\r]+$/,"",$2);print $2}')
+    echo "$_dapiURL"
+}
+
+_test_data_source() {
+    response=$(curl -o /dev/null -s -w "%{http_code}:%{time_total}s\n" --request POST "$1" \
+      --header 'Content-Type: application/json' \
+      --data-raw '{
+        "jsonrpc": "2.0",
+        "method": "eth_getBlockByNumber",
+        "params": [
+            "latest",
+            true
+        ],
+        "id": 1
+    }' -k)
+    echo "Datasource response: $response"
+    # if [[ "$dapi_response_code" != "200" ]]; then
+    #   echo "Calling dAPI: Failed"
+    #   exit 1
+    # fi
+}
+_test_node_api() {
+    response=$(curl -o /dev/null -s -w "%{http_code}:%{time_total}s\n" --request POST "https://$1" \
+      --header 'Content-Type: application/json' \
+      --header "Host: $2.node.mbr.$domain" \
+      --header "X-Api-Key: $3" \
+      --data-raw '{
+        "jsonrpc": "2.0",
+        "method": "eth_getBlockByNumber",
+        "params": [
+            "latest",
+            true
+        ],
+        "id": 1
+    }' -k -vv)
+    echo "Node response: $response"
+    # if [[ "$dapi_response_code" != "200" ]]; then
+    #   echo "Calling dAPI: Failed"
+    #   exit 1
+    # fi
+}
+
+_test_gateway_api() {
+    #echo "|$1|$2|$3|"
+    #echo "Test host $2.gw.mbr.$domain"
+    # curl --request POST "https://$1" \
+    #   --header 'Content-Type: application/json' \
+    #   --header "Host: $2.gw.mbr.$domain" \
+    #   --header "X-Api-Key: $3" \
+    #   --data-raw '{
+    #     "jsonrpc": "2.0",
+    #     "method": "eth_getBlockByNumber",
+    #     "params": [
+    #         "latest",
+    #         true
+    #     ],
+    #     "id": 1
+    # }' -k
+    response=$(curl -o /dev/null -s -w "%{http_code}:%{time_total}s\n" --request POST "https://$1" \
+      --header 'Content-Type: application/json' \
+      --header "Host: $2.gw.mbr.$domain" \
+      --header "X-Api-Key: $3" \
+      --data-raw '{
+        "jsonrpc": "2.0",
+        "method": "eth_getBlockByNumber",
+        "params": [
+            "latest",
+            true
+        ],
+        "id": 1
+    }' -k -vv)
+    echo "Gateway response: $response"
+}
+_test_dapi() {
+    response=$(curl -o /dev/null -s -w "%{http_code}:%{time_total}s\n" --request POST "$1" \
+      --header 'Content-Type: application/json' \
+      --data-raw '{
+        "jsonrpc": "2.0",
+        "method": "eth_getBlockByNumber",
+        "params": [
+            "latest",
+            true
+        ],
+        "id": 1
+    }' -L)
+    echo "Dapi response: $response"
+    # if [[ "$response_code" != "200" ]]; then
+    #   echo "Calling dAPI: Failed"
+    #   exit 1
+    # fi
+    # echo "Calling dAPI: Pass"
+}
+_benchmark() {
+  for rate in "${rates[@]}"
+    do
+      $wrk_dir/wrk -t$thread -c$connection -d$duration -R$rate --latency -T$timeout -s $wrk_dir/massbit.lua $1 -- $2 $3 $4 > $output
+      latency_row=$(cat $output  | grep -A 4 "Thread Stats   Avg      Stdev     Max   +/- Stdev" | sed -n "2 p")
+      IFS='    ' read -ra latency <<< "$latency_row"
+      req_sec_row=$(cat $output  | grep -A 4 "Thread Stats   Avg      Stdev     Max   +/- Stdev" | sed -n "3 p")
+      IFS='    ' read -ra req_sec <<< "$req_sec_row"
+      hdrhistogram50=$(cat $output  | grep -A 4 "Latency Distribution (HdrHistogram - Recorded Latency)" | sed -n "2 p")
+      IFS='    ' read -ra hdrhistogram50 <<< "$hdrhistogram50"
+      hdrhistogram75=$(cat $output  | grep -A 4 "Latency Distribution (HdrHistogram - Recorded Latency)" | sed -n "3 p")
+      IFS='    ' read -ra hdrhistogram75 <<< "$hdrhistogram75"
+      hdrhistogram90=$(cat $output  | grep -A 4 "Latency Distribution (HdrHistogram - Recorded Latency)" | sed -n "4 p")
+      IFS='    ' read -ra hdrhistogram90 <<< "$hdrhistogram90"
+      hdrhistogram99=$(cat $output  | grep -A 4 "Latency Distribution (HdrHistogram - Recorded Latency)" | sed -n "5 p")
+      IFS='    ' read -ra hdrhistogram99 <<< "$hdrhistogram99"
+      cat $output
+      curl 'https://docs.google.com/forms/d/1gzn6skD5MH7D3cyIsv8qcbkbox6QRcxzhkT9AomXE8o/formResponse' --silent >/dev/null \
+        --data "entry.721172135=$2&entry.1670770464=$zone&entry.1360977389=$blockchain&entry.1089136036=$duration&entry.770798199=$rate&entry.144814654=${latency[1]}&entry.542037870=${latency[2]}&entry.1977269592=${latency[3]}&entry.1930208986=${hdrhistogram75[1]}&entry.1037348686=${hdrhistogram90[1]}&entry.131454525=${hdrhistogram99[1]}&entry.1567713965=${req_sec[1]}"
+    done
+}
+
+_run() {
+  echo "Get dapiURL with session"
+  _dapiURL=$(_get_dapi_session $dapiURL)
+  echo $_dapiURL
+  declare -A codes
+
+  #_test_data_source $datasourceUrl
+  _test_node_api $nodeIp $nodeId $nodeKey
+  echo "Node response $?"
+  _test_gateway_api $gatewayIp $gatewayId $gatewayKey
+  echo "Gateway response $?"
+  _test_dapi $_dapiURL
+
+  echo "Benchmarking datasource $datasourceUrl ..."
+  _benchmark $datasourceUrl datasource
+
+  nodeUrl="https://$nodeIp"
+  echo "Benchmarking node $nodeUrl ..."
+  _benchmark $nodeUrl node $nodeId $nodeKey
+
+  gatewayUrl="https://$gatewayIp"
+  echo "Benchmarking gateway $gatewayUrl ..."
+  _benchmark "$gatewayUrl" gateway $gatewayId $gatewayKey
+
+  echo "Benchmarking dapi $_dapiURL ..."
+  _benchmark "$_dapiURL" dapi
+}
+
+$@
