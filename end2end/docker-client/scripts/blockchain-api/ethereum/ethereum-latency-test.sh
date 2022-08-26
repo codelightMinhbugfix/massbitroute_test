8f105e39ee99f2f8aba621c969a75fd4a5e4e482
Ngo Duong Kha <ngoduongkhakg2001@gmail.com>
2022-08-26 16:17:35 +0700

    chore: fix http status code

--- end2end/docker-client/scripts/blockchain-api/ethereum/ethereum-latency-test.sh
+++ end2end/docker-client/scripts/blockchain-api/ethereum/ethereum-latency-test.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-
-ethereum_api=$(cat ./ethereum/input/ethereum-latency-api.json)
+ROOT_DIR=$(realpath $(dirname $(realpath $0)))
+ethereum_api=$(cat $ROOT_DIR/input/ethereum-latency-api.json)
 
 failed=0
 passed=0
@@ -11,6 +11,30 @@ for i in $(seq 1 $NUMBER_OF_TESTS); do
     params=$(echo $ethereum_api | jq .[$j].params)
 
     body="{\"jsonrpc\": \"2.0\", \"method\": $method, \"params\": $params, \"id\": 67}"
+
+    massbit_http_code=$(curl $MASSBIT_ROUTE_ETHEREUM \
+      --silent \
+      --header "Content-Type: application/json" \
+      --request POST \
+      --data "$body" \
+      -o /dev/null -s -w "%{http_code}\n")
+    another_provider_http_code=$(curl $ANOTHER_ETHEREUM_PROVIDER \
+      --silent \
+      --header "Content-Type: application/json" \
+      --request POST \
+      --data "$body" \
+      -o /dev/null -s -w "%{http_code}\n")
+
+    if ! [[ "$massbit_http_code" =~ ^20[01]$ && "$another_provider_http_code" =~ ^20[01]$ ]]; then
+      error=$((error + 1))
+      error_report=$(echo $error_report | jq ". += [{\"method\":$method, \"response\": \"Massbit http code: $massbit_http_code\", \"expectedResponse\": \"Another provider http code: $another_provider_http_code\"}]")
+
+      echo "==================== ERROR ==================="
+      echo "method : $method"
+      echo "Massbit http code: $massbit_http_code"
+      echo "Another provider http code: $another_provider_http_code"
+      continue
+    fi
 
     response=$(curl $MASSBIT_ROUTE_ETHEREUM \
       --silent \
